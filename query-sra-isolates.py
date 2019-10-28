8e410e7374a137f4dda4bef6629a5dcbf39f3888
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-10-28 18:46:19 -0400

    combine samples

--- query-sra-isolates.py
+++ query-sra-isolates.py
@@ -1,6 +1,11 @@
-From Bio import Entrez
+from Bio import Entrez
 
-Entrez.email = "Your.Name.Here@example.org"
-handle = Entrez.efetch(db="bioproject",id="",rettype = "xml", retmode = "xml")
+sra_uid = "/Users/rx32940/Dropbox/5. Rachel's projects/Phylogeography/bioprojects_uids.txt"
+with open(sra_uid) as f:
+    ids=f.read().split('\n')
+
+
+Entrez.email = "rx32940@uga.edu"
+handle = Entrez.efetch(db="bioproject",id=ids,rettype = "xml", retmode = "xml")
 
 Entrez.read(handle)
