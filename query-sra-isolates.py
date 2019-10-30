e9a0ad8b50c7116720ac006ff68872ef809f2f2c
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-10-30 15:19:51 -0400

    use sra ran selector instead

--- query-sra-isolates.py
+++ query-sra-isolates.py
@@ -1,11 +1,15 @@
 from Bio import Entrez
 
-sra_uid = "/Users/rx32940/Dropbox/5. Rachel's projects/Phylogeography/bioprojects_uids.txt"
-with open(sra_uid) as f:
-    ids=f.read().split('\n')
+sra_uid = "/Users/rx32940/Dropbox/5.Rachel-projects/Phylogeography/bioprojects_uids.txt"
 
+with open(sra_uid) as f:
+    ids= f.read().split('\n')
+
+ids = [int(i) for i in ids]
 
 Entrez.email = "rx32940@uga.edu"
-handle = Entrez.efetch(db="bioproject",id=ids,rettype = "xml", retmode = "xml")
+handle = Entrez.esummary(db="bioproject",id=477299,retmode="xml",rettype="xml")
 
-Entrez.read(handle,validate=False)
+results = Entrez.read(handle,validate=False)
+
+print(results)
