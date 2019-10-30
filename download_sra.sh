4c2fb066d514d38ad723030ba105729a6be39852
rx32940 <31255012+rx32940@users.noreply.github.com>
2019-10-30 17:04:19 -0400

    download all sra seq

---
+++ download_sra.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -q batch                                                            
+#PBS -N lepto_fastq                                            
+#PBS -l nodes=1:ppn=4 -l mem=100gb                                        
+#PBS -l walltime=100:00:00                                                
+#PBS -M rx32940@uga.edu                                                  
+#PBS -m abe                                                              
+#PBS -o /scratch/rx32940                        
+#PBS -e /scratch/rx32940                        
+#PBS -j oe     
+
+cd $PBS_O_WORKDIR
+
+module load SRA-Toolkit/2.9.1-centos_linux64
+
+cat /scratch/rx32940/sra_date_wgs.txt | xargs -I{} fastq-dump --gzip -O /scratch/rx32940/LeptoFastqSRA {}
