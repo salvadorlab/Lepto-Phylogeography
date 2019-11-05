4d67c68c2723cbaaf45e30415da8c0e08b68e8ac
Rachel Xu <rx32940@uga.edu>
2019-11-05 14:12:00 -0500

    commit

--- query-sra-isolates.py
+++ query-sra-isolates.py
@@ -1,6 +1,11 @@
-From Bio import Entrez
+from Bio import Entrez
+
+file = "/Users/rachel/Dropbox/5.Rachel-projects/Phylogeography/bioprojects_uids.txt"
+
+with open(file) as f:
+    ids = f.read().split("\n")
 
 Entrez.email = "Your.Name.Here@example.org"
-handle = Entrez.efetch(db="bioproject",id="",rettype = "xml", retmode = "xml")
+handle = Entrez.efetch(db="nucleotide",id=ids,retmode = "xml")
 
-Entrez.read(handle)
+Entrez.read(handle,validate=False)
