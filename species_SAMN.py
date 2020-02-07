51cd42443c0a0df5b2c23e05a3edd1158fabd01e
rx32940 <31255012+rx32940@users.noreply.github.com>
2020-02-07 11:25:51 -0500

    start new file each time

--- species_SAMN.py
+++ species_SAMN.py
@@ -3,11 +3,14 @@ import xml.etree.ElementTree as ET
 # https://docs.python.org/2/library/xml.etree.elementtree.html
 
 def main():
-
+    
     path = "/Users/rx32940/Downloads/"
-    doc = ET.parse(path + "biosample_result-2.xml") # xml object, biosample result file downloaded directly from ncbi with all_assemblies list
+    doc = ET.parse(path + "biosample_result-3.xml") # xml object, biosample result file downloaded directly from ncbi with all_assemblies list
     root = doc.getroot() # get the root element
     
+    with open(path + "SAMN_Organism_1209.txt","w") as f:
+        f.write("")
+        
     for des in root.findall("BioSample"):
         SAMN_id = des.attrib["accession"]
         organisms_name = des.find("Description").find("Organism").attrib["taxonomy_name"] # access nested nodes
