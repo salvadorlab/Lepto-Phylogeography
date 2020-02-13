fccdb46819934d90280e3d6cf9c1038f4141bef9
rx32940 <rx32940@uga.edu>
2020-02-13 17:54:23 -0500

    commit

--- rename_quast_report.py
+++ rename_quast_report.py
@@ -1,8 +1,8 @@
 import os
 
-path = "/scratch/rx32940/All_Lepto_Assemblies/PATRIC_assemblies_633/quast_assemblies/"
+path = "/scratch/rx32940/All_Lepto_Assemblies/dated_assembled_51/quast_assemblies/"
 
 for dir in os.listdir(path):
     dir_name = str(dir)
-    os.rename(path + dir+"/report.tsv", path + dir+ "/" + dir + ".tsv")
+    os.rename(path + dir+"/transposed_report.tsv", path + dir+ "/" + dir + ".tsv")
     
