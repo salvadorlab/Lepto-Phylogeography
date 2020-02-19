3839f6684b9886ef4fdc6065e36411e22bdc71e0
rx32940 <rx32940@uga.edu>
2020-02-19 16:06:29 -0500

    trim

--- dated_assemblies_pipeline.sh
+++ dated_assemblies_pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -q highmem_q                                                            
-#PBS -N dated51_pipeline                                        
+#PBS -N trim_double                                        
 #PBS -l nodes=1:ppn=12 -l mem=10gb                                        
 #PBS -l walltime=100:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
@@ -32,9 +32,50 @@
 #
 # #####################################################################
 
-path_qc="/scratch/rx32940/Lepto_Work/fastqc" 
+# path_qc="/scratch/rx32940/Lepto_Work/fastqc" 
 
-module load MultiQC/1.5-foss-2016b-Python-2.7.14
+# module load MultiQC/1.5-foss-2016b-Python-2.7.14
 
-multiqc $path_qc/*_fastqc.zip -o $path_qc
+# multiqc $path_qc/*_fastqc.zip -o $path_qc
+
+############################################################################
+# 
+# trimming 
+# Trimmomatic
+# single end and pair end fastq files need to trim separately with separate adapters
+# 
+############################################################################
+
+
+module load Trimmomatic/0.36-Java-1.8.0_144
+
+# trim single end files
+path_seq="/scratch/rx32940/merged_runs"
+output_dir="/scratch/rx32940/Lepto_Work/trim"
+
+# for fastq in $path_seq/single/*;
+# do
+#     biosample=$(basename "$fastq" .fastq.gz)
+#     java -jar /usr/local/apps/eb/Trimmomatic/0.36-Java-1.8.0_144/trimmomatic-0.36.jar \
+#     SE -threads 4 $path_seq/single/$biosample.fastq.gz $output_dir/single/${biosample}_trimmed.fastq.gz \
+#     ILLUMINACLIP:/usr/local/apps/eb/Trimmomatic/0.36-Java-1.8.0_144/adapters/TruSeq3-SE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+# done
+
+cat $path_seq/pair.txt | \
+while read biosample;
+do  
+    java -jar /usr/local/apps/eb/Trimmomatic/0.36-Java-1.8.0_144/trimmomatic-0.36.jar \
+    PE -threads 4 $path_seq/pair/${biosample}_1.fastq.gz $path_seq/pair/${biosample}_2.fastq.gz \
+    $output_dir/pair/${biosample}_1_paired_trimmed.fastq.gz $output_dir/pair/${biosample}_1_unpaired_trimmed.fastq.gz \
+    $output_dir/pair/${biosample}_2_paired_trimmed.fastq.gz $output_dir/pair/${biosample}_2_unpaired_trimmed.fastq.gz \
+    ILLUMINACLIP:/usr/local/apps/eb/Trimmomatic/0.36-Java-1.8.0_144/adapters/TruSeq3-PE.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+done
+
+############################################################################
+#
+# SPADES
+# Assemble the trimmed seq
+#
+############################################################################
+
 
