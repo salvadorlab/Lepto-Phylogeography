787ed0d7c4b1cd40dd6dc30e79696c2e401902ec
rx32940 <rx32940@uga.edu>
2020-07-01 17:12:31 -0400

    finding core with COG and BDBH algorithm

--- get_homo.sh
+++ get_homo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #PBS -q bahl_salv_q                                                            
-#PBS -N COG_core                                    
+#PBS -N BDBH_core                                    
 #PBS -l nodes=1:ppn=64
 #PBS -l mem=100gb                                        
-#PBS -l walltime=20:00:00                                                
+#PBS -l walltime=200:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
 #PBS -o /scratch/rx32940                        
@@ -13,7 +13,7 @@
 cd /scratch/rx32940/get_homo/
 
 
-ml GETHOMOLOGUES/1.7.6
+# ml GETHOMOLOGUES/1.7.6
 module load R/4.0.0-foss-2019b
 module load GD/2.66-foss-2016b-Perl-5.24.1
 # default aglorithm doesn't have -t 0 option available (see manual section 4.8)
@@ -22,8 +22,8 @@ module load GD/2.66-foss-2016b-Perl-5.24.1
 #       1.1) used -o for blast search only, delete -o for orthologous genes clustering after blast
 # 2) remove -o, do COG orthologous analysis
 # 3) use -c for genome composition analysis, produce tab file for parse_pangenome_matrix.pl
-/scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/gbk -G -n 64 -c
-
+# /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/gbk -G -n 64 -c -z
+/scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/gbk_BDBH -n 64 -c -z
 
 # use OMCL algorithm with all possible clusters
 # get_homologues.pl -d /scratch/rx32940/get_homo/gbk_dup -t 0 -M -n 48
