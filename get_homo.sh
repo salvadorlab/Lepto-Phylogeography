8f13947721c75cf26d81cb094c88a9a31f5440cb
rx32940 <rx32940@uga.edu>
2020-07-15 08:35:50 -0400

    current: latin core, rep OMCL (COG done, need intersection before core)

--- get_homo.sh
+++ get_homo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #PBS -q bahl_salv_q                                                            
-#PBS -N bdbh_core                                    
-#PBS -l nodes=1:ppn=64
+#PBS -N COG_core                                    
+#PBS -l nodes=1:ppn=2
 #PBS -l mem=100gb                                        
-#PBS -l walltime=300:00:00                                                
+#PBS -l walltime=500:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
 #PBS -o /scratch/rx32940                        
@@ -22,11 +22,17 @@ module load GD/2.66-foss-2016b-Perl-5.24.1
 #       1.1) used -o for blast search only, delete -o for orthologous genes clustering after blast
 # 2) remove -o, do COG orthologous analysis
 # 3) use -c for genome composition analysis, produce tab file for parse_pangenome_matrix.pl
-# /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/gbk -G -n 64 -c -z
-/scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/gbk_bdbh -n 64 -c -z
+/scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/gbk -G -n 64 -c -z
+# /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/gbk_bdbh -n 64 -c -z
 
 # use OMCL algorithm with all possible clusters
 # get_homologues.pl -d /scratch/rx32940/get_homo/gbk_dup -t 0 -M -n 48
 
+# use BDBH to get the orthologous genes for all latino and caribbean isolates
+# /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/latin_isolates/gbk_latin_bdbh -n 24 -c -z
+
 # classify pangenome into four departments
 # parse_pangenome_matrix.pl -m /scratch/rx32940/get_homo/gbk_homologues/SAMN02947784_f0_0taxa_algCOG_e0_ -s
+
+# pangenome analysis for 106 representative isolates with COG algorithm (manual 4.8.1)
+# /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/pangenome/gbk_subset -n 24 -t 0 -M
