856e0e8cf0a11a54e3793881d1e8f08a25e950e5
rx32940 <rx32940@uga.edu>
2020-07-16 16:32:52 -0400

    compare COG OMCL cluster for rep isolates

--- get_homo.sh
+++ get_homo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -q bahl_salv_q                                                            
-#PBS -N COG_core                                    
+#PBS -N rep_inter                                    
 #PBS -l nodes=1:ppn=2
 #PBS -l mem=100gb                                        
 #PBS -l walltime=500:00:00                                                
@@ -22,7 +22,7 @@ module load GD/2.66-foss-2016b-Perl-5.24.1
 #       1.1) used -o for blast search only, delete -o for orthologous genes clustering after blast
 # 2) remove -o, do COG orthologous analysis
 # 3) use -c for genome composition analysis, produce tab file for parse_pangenome_matrix.pl
-/scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/gbk -G -n 64 -c -z
+# /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/gbk -G -n 64 -c -z
 # /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/gbk_bdbh -n 64 -c -z
 
 # use OMCL algorithm with all possible clusters
@@ -36,3 +36,5 @@ module load GD/2.66-foss-2016b-Perl-5.24.1
 
 # pangenome analysis for 106 representative isolates with COG algorithm (manual 4.8.1)
 # /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/pangenome/gbk_subset -n 24 -t 0 -M
+
+/scratch/rx32940/get_homo/get_homologues-x86_64-20170302/compare_clusters.pl -m -o gbk_subset_intersection -d gbk_subset_homologues/SAMN02603616_f0_0taxa_algCOG_e0_, SAMN02603616_f0_0taxa_algOMCL_e0_
