58b06d8e6d4f96b8ead1246a86da56f72f5289ad
rx32940 <rx32940@uga.edu>
2020-07-19 14:52:10 -0400

    pangenome analysis get_homo 106 isolates

--- get_homo.sh
+++ get_homo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -q batch                                                            
-#PBS -N rep_intersect                                    
+#PBS -N rep_core                                    
 #PBS -l nodes=1:ppn=2
 #PBS -l mem=100gb                                        
 #PBS -l walltime=300:00:00                                                
@@ -14,8 +14,12 @@ cd /scratch/rx32940/get_homo/
 
 
 # ml GETHOMOLOGUES/1.7.6
-module load R/4.0.0-foss-2019b
-module load GD/2.66-foss-2016b-Perl-5.24.1
+
+module load GD/2.69-GCCcore-7.3.0-Perl-5.28.0
+module load BioPerl/1.7.1-foss-2016b-Perl-5.24.1
+module load GCCcore/5.4.0
+Rscript /scratch/rx32940/load_packages.r
+
 # default aglorithm doesn't have -t 0 option available (see manual section 4.8)
 
 # 1) use COG algorithm with all possible clusters (even thus which might not contain sequences from all input genomes (taxa)) -t 0 to report all clusters
@@ -42,7 +46,12 @@ module load GD/2.66-foss-2016b-Perl-5.24.1
 
 
 
-# pangenome analysis for 106 representative isolates with COG algorithm (manual 4.8.1)
-# /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/pangenome/gbk_subset -n 24 -t 0 -M
+# pangenome analysis for 106 representative isolates with COG/OMCL algorithm (manual 4.8.1)
+/scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/pangenome/gbk_subset -n 24 -t 0 -M
+/scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/pangenome/gbk_subset -n 24 -t 0 -G
 
+# get the intersection of clusters analyzed with the two differen clusters
 /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/compare_clusters.pl -m -o gbk_subset_intersection -d gbk_subset_homologues/SAMN02603616_f0_0taxa_algCOG_e0_,gbk_subset_homologues/SAMN02603616_f0_0taxa_algOMCL_e0_
+
+# analyze the intersected pangenome of representative isolates
+/scratch/rx32940/get_homo/get_homologues-x86_64-20170302/parse_pangenome_matrix.pl -m /scratch/rx32940/get_homo/gbk_subset_intersection/pangenome_matrix_t0.tab -s
