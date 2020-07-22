bde6e7d458054b551fead649937f17d8ff72a9c7
rx32940 <rx32940@uga.edu>
2020-07-21 20:45:32 -0400

    reset get_homo output dir

--- get_homo.sh
+++ get_homo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q batch                                                            
-#PBS -N rep_core                                    
-#PBS -l nodes=1:ppn=2
+#PBS -N COG_ANI                                    
+#PBS -l nodes=1:ppn=24
 #PBS -l mem=100gb                                        
 #PBS -l walltime=300:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
@@ -10,7 +10,7 @@
 #PBS -e /scratch/rx32940                        
 #PBS -j oe
 
-cd /scratch/rx32940/get_homo/
+
 
 
 # ml GETHOMOLOGUES/1.7.6
@@ -18,7 +18,9 @@ cd /scratch/rx32940/get_homo/
 module load GD/2.69-GCCcore-7.3.0-Perl-5.28.0
 module load BioPerl/1.7.1-foss-2016b-Perl-5.24.1
 module load GCCcore/5.4.0
-Rscript /scratch/rx32940/load_packages.r
+# Rscript /scratch/rx32940/load_packages.r
+
+corepath="/scratch/rx32940/get_homo"
 
 # default aglorithm doesn't have -t 0 option available (see manual section 4.8)
 
@@ -26,32 +28,41 @@ Rscript /scratch/rx32940/load_packages.r
 #       1.1) used -o for blast search only, delete -o for orthologous genes clustering after blast
 # 2) remove -o, do COG orthologous analysis
 # 3) use -c for genome composition analysis, produce tab file for parse_pangenome_matrix.pl
-# /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/gbk -G -n 64 -c -z
-# /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/gbk_bdbh -n 64 -c -z
+# $corepath/get_homologues-x86_64-20170302/get_homologues.pl -d $corepath/gbk -G -n 64 -c -z
+# $corepath/get_homologues-x86_64-20170302/get_homologues.pl -d $corepath/gbk_bdbh -n 64 -c -z
 
 # use OMCL algorithm with all possible clusters
-# get_homologues.pl -d /scratch/rx32940/get_homo/gbk_dup -t 0 -M -n 48
+# get_homologues.pl -d $corepath/gbk_dup -t 0 -M -n 48
 
 # use BDBH to get the orthologous genes for all latino and caribbean isolates
-# /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/get_homo/latin_isolates/gbk_latin_bdbh -n 24 -c -z
+# $corepath/get_homologues-x86_64-20170302/get_homologues.pl -d $corepath/latin_isolates/gbk_latin_bdbh -n 24 -c -z
 
-# /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/parse_pangenome_matrix.pl -m /scratch/rx32940/get_homo/gbk_latin_bdbh_homologues/SAMN11389084_f0_alltaxa_algBDBH_e0_ -s
+# $corepath/get_homologues-x86_64-20170302/parse_pangenome_matrix.pl -m $corepath/gbk_latin_bdbh_homologues/SAMN11389084_f0_alltaxa_algBDBH_e0_ -s
 
 
 # classify pangenome into four departments
-# /scratch/rx32940/get_homo/get_homologues-x86_64-20170302/parse_pangenome_matrix.pl -m /scratch/rx32940/get_homo/gbk_homologues/SAMN02947784_f0_0taxa_algCOG_e0_ -s
+# $corepath/get_homologues-x86_64-20170302/parse_pangenome_matrix.pl -m $corepath/gbk_homologues/SAMN02947784_f0_0taxa_algCOG_e0_ -s
 
 
 
 
+panpath="/scratch/rx32940/pangenome"
+cd $panpath
 
+# # pangenome analysis for 106 representative isolates with COG/OMCL algorithm (manual 4.8.1)
+# # -A, calculate ANI identity with the BLASTP scores among protein sequences
+# $corepath/get_homologues-x86_64-20170302/get_homologues.pl -d $panpath/gbk_subset -n 24 -t 0 -M -A
+$corepath/get_homologues-x86_64-20170302/get_homologues.pl -d $panpath/gbk_subset -n 24 -t 0 -G -A
 
-# pangenome analysis for 106 representative isolates with COG/OMCL algorithm (manual 4.8.1)
-/scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/pangenome/gbk_subset -n 24 -t 0 -M
-/scratch/rx32940/get_homo/get_homologues-x86_64-20170302/get_homologues.pl -d /scratch/rx32940/pangenome/gbk_subset -n 24 -t 0 -G
+# plot ANI matrix 
+# -d, number of decimal places
+# $corepath/get_homologues-x86_64-20170302/plot_matrix_heatmap.sh -i $panpath/gbk_subset -d 2 
 
-# get the intersection of clusters analyzed with the two differen clusters
-/scratch/rx32940/get_homo/get_homologues-x86_64-20170302/compare_clusters.pl -m -o gbk_subset_intersection -d gbk_subset_homologues/SAMN02603616_f0_0taxa_algCOG_e0_,gbk_subset_homologues/SAMN02603616_f0_0taxa_algOMCL_e0_
+# # get the intersection of clusters analyzed with the two differen clusters
+# $corepath/get_homologues-x86_64-20170302/compare_clusters.pl -m -o $panpath/gbk_subset_intersection -d $panpath/gbk_subset_homologues/SAMN02603616_f0_0taxa_algCOG_e0_,$panpath/gbk_subset_homologues/SAMN02603616_f0_0taxa_algOMCL_e0_
 
-# analyze the intersected pangenome of representative isolates
-/scratch/rx32940/get_homo/get_homologues-x86_64-20170302/parse_pangenome_matrix.pl -m /scratch/rx32940/get_homo/gbk_subset_intersection/pangenome_matrix_t0.tab -s
+# # analyze the intersected pangenome of representative isolates
+# $corepath/get_homologues-x86_64-20170302/parse_pangenome_matrix.pl -m $panpath/gbk_subset_intersection/pangenome_matrix_t0.tab -s
+
+# plot pangenome curve
+# $corepath/get_homologues-x86_64-20170302/plot_pancore_matrix.pl -i $panpath/gbk_subset_intersection/pangenome_matrix_t0.tab -f core_Tettelin
--- run_pirate.sh
+++ run_pirate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#PBS -q batch                                                            
-#PBS -N pirate_date_mi4                                        
-#PBS -l nodes=1:ppn=24 -l mem=1000gb                                        
-#PBS -l walltime=100:00:00                                                
+#PBS -q bahl_salv_q                                                            
+#PBS -N pirate_mi4_iqtree                                        
+#PBS -l nodes=1:ppn=64 -l mem=100gb                                        
+#PBS -l walltime=300:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
 #PBS -o /scratch/rx32940                        
@@ -11,14 +11,14 @@
 
 # run pirate to find the core genome of the leptospira isolates with collection date
 # increase the mcl inflation value to 6 according to the software literature
-/home/rx32940/miniconda3/bin/PIRATE -i /scratch/rx32940/pirate/dated_lepto -o /scratch/rx32940/pirate/dated_output_mi4 -a -r -t 12 -pan-opt "-f 6"
+# /home/rx32940/miniconda3/bin/PIRATE -i /scratch/rx32940/pirate/dated_lepto -o /scratch/rx32940/pirate/dated_output_mi4 -a -r -t 12 -pan-opt "-f 6"
 
 
 module load IQ-TREE/1.6.5-omp
 
 # use MFP: model finder to find the right substitution model
 # -nt AUTO, detects best number of cores to use
-iqtree -nt AUTO -m MFP -pre /scratch/rx32940/pirate/iqtree_mi6/iqtree_mi6 -s /scratch/rx32940/pirate/dated_output_mi6/core_alignment.fasta -o SAMN02947961
+iqtree -nt 64 -m MFP -pre /scratch/rx32940/pirate/iqtree_mi6/iqtree_mi6 -s /scratch/rx32940/pirate/dated_output_mi6/core_alignment.fasta -o SAMN02947961
 
 # bootstrap for ML tree 1000 times confidence level
-iqtree -m -pre /scratch/rx32940/pirate/iqtree_mi6/iqtree_mi6 -s /scratch/rx32940/pirate/dated_output_mi6/core_alignment.fasta -o SAMN02947961 -nt AUTO -B 1000
+# iqtree -m -pre /scratch/rx32940/pirate/iqtree_mi6/iqtree_mi6 -s /scratch/rx32940/pirate/dated_output_mi6/core_alignment.fasta -o SAMN02947961 -nt AUTO -B 1000
