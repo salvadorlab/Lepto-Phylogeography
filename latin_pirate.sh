e011f40516c49f3f6579c9275e4f66f4167b1c14
rx32940 <rx32940@uga.edu>
2020-07-27 13:38:20 -0400

    running latin iqtree again w/o outgroup

--- latin_pirate.sh
+++ latin_pirate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -q batch                                                            
-#PBS -N latin_pirate                                        
+#PBS -N latin_iqtree                                        
 #PBS -l nodes=1:ppn=24 -l mem=100gb                                        
 #PBS -l walltime=300:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
@@ -13,12 +13,12 @@
 ########## Run Pirate for Core/Pan genome analysis ################################################
 # run pirate to find the core genome of the leptospira isolates with collection date
 # increase the mcl inflation value to 6 according to the software literature
-/home/rx32940/miniconda3/bin/PIRATE -i /scratch/rx32940/pirate/latin_lepto/latin_prokka -o /scratch/rx32940/pirate/latin_lepto/latin_output/ -a -r -t 12 -pan-opt "-f 6"
+# /home/rx32940/miniconda3/bin/PIRATE -i /scratch/rx32940/pirate/latin_lepto/latin_prokka -o /scratch/rx32940/pirate/latin_lepto/latin_output/ -a -r -t 12 -pan-opt "-f 6"
 
 
 ########## ML tree with core gene sequences alignment ##############################################
-# module load IQ-TREE/1.6.5-omp
+module load IQ-TREE/1.6.5-omp
 
 # use MFP: model finder to find the right substitution model
 # -nt AUTO, detects best number of cores to use
-# iqtree -nt AUTO -m MFP -pre /scratch/rx32940/pirate/latin_lepto/iqtree_latin/iqtree_latin -s /scratch/rx32940/pirate/latin_lepto/latin_output/core_alignment.fasta -o SAMN02947961
+iqtree -nt AUTO -m MFP -pre /scratch/rx32940/pirate/latin_lepto/iqtree_latin/iqtree_latin -s /scratch/rx32940/pirate/latin_lepto/latin_output/core_alignment.fasta
