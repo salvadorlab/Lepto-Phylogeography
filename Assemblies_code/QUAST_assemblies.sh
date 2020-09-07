9499d07ed58fbe826acd950d92396abe16f26250
rx32940 <rx32940@uga.edu>
2020-09-07 13:59:32 -0400

    change quast dir

--- Assemblies_code/QUAST_assemblies.sh
+++ Assemblies_code/QUAST_assemblies.sh
@@ -10,16 +10,19 @@
 #PBS -j oe  
 
 module load QUAST/5.0.2-foss-2018a-Python-2.7.14
-
 QUASTPATH="/scratch/rx32940/pirate/all_dated" 
-cat /scratch/rx32940/pirate/dated_lepto_389.txt  | \
-xargs -I{} quast.py \
-$QUASTPATH/all_dated_assemblies/{}.fasta \
--o $QUASTPATH/all_dated_quast/{} \
--t 12 
 
-# module load MultiQC/1.5-foss-2016b-Python-2.7.14
-# multiqc 
+# cat /scratch/rx32940/pirate/dated_lepto_389.txt  | \
+# xargs -I{} quast.py \
+# $QUASTPATH/all_dated_assemblies/{}.fasta \
+# -o $QUASTPATH/all_dated_quast/{} \
+# -t 12 
+
+module load MultiQC/1.5-foss-2016b-Python-2.7.14
+multiqc $QUASTPATH/all_dated_quast/*/report.tsv \
+-d -dd 1 -o $QUASTPATH/all_dated_quast \
+-n all_dated_388_quast
+
 # quast.py $QUASTPATH/All_Lepto_Assemblies/dated_assembled_51/assemblies/SAMN03944914/scaffolds.fasta -o $QUASTPATH/All_Lepto_Assemblies/dated_assembled_51/quast_assemblies/SAMN03944914 \
 # -r $QUASTPATH/reference/GCF_000092565.1_ASM9256v1_genomic.fna \
 # -g $QUASTPATH/reference/GCF_000092565.1_ASM9256v1_genomic.gff \
