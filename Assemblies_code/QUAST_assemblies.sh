54ca4f1eeb88182c1a754ec970cbfaa2cc282538
rx32940 <rx32940@uga.edu>
2020-09-07 13:16:11 -0400

    rerun pirate for core genome w/o dup, quast all dated assemblies

--- Assemblies_code/QUAST_assemblies.sh
+++ Assemblies_code/QUAST_assemblies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -q highmem_q                                                            
-#PBS -N QUAST_51                                      
-#PBS -l nodes=1:ppn=8 -l mem=100gb                                        
+#PBS -q batch                                                            
+#PBS -N QUAST_all_dated                                      
+#PBS -l nodes=1:ppn=12 -l mem=100gb                                        
 #PBS -l walltime=100:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
@@ -11,14 +11,15 @@
 
 module load QUAST/5.0.2-foss-2018a-Python-2.7.14
 
-QUASTPATH="/scratch/rx32940" 
-cat $QUASTPATH/All_Lepto_Assemblies/dated_assembled_51/dated_assemblies_51.txt  | xargs -I{} quast.py \
-$QUASTPATH/All_Lepto_Assemblies/dated_assembled_51/assemblies/{}/scaffolds.fasta \
--o $QUASTPATH/All_Lepto_Assemblies/dated_assembled_51/quast_assemblies/{} \
--r $QUASTPATH/reference/GCF_000092565.1_ASM9256v1_genomic.fna \
--g $QUASTPATH/reference/GCF_000092565.1_ASM9256v1_genomic.gff
--t 8 
+QUASTPATH="/scratch/rx32940/pirate/all_dated" 
+cat /scratch/rx32940/pirate/dated_lepto_389.txt  | \
+xargs -I{} quast.py \
+$QUASTPATH/all_dated_assemblies/{}.fasta \
+-o $QUASTPATH/all_dated_assemblies/all_dated_quast/{} \
+-t 12 
 
+# module load MultiQC/1.5-foss-2016b-Python-2.7.14
+# multiqc 
 # quast.py $QUASTPATH/All_Lepto_Assemblies/dated_assembled_51/assemblies/SAMN03944914/scaffolds.fasta -o $QUASTPATH/All_Lepto_Assemblies/dated_assembled_51/quast_assemblies/SAMN03944914 \
 # -r $QUASTPATH/reference/GCF_000092565.1_ASM9256v1_genomic.fna \
 # -g $QUASTPATH/reference/GCF_000092565.1_ASM9256v1_genomic.gff \
--- subsample_pirate_iqtree.sh
+++ subsample_pirate_iqtree.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -q bahl_salv_q                                                          
-#PBS -N core_subppk_iqtree                                       
-#PBS -l nodes=1:ppn=64 -l mem=80gb                                        
+#PBS -q batch                                                          
+#PBS -N core_subppk_pirate                                       
+#PBS -l nodes=1:ppn=12 -l mem=100gb                                        
 #PBS -l walltime=300:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
@@ -12,12 +12,12 @@
 # create an alignment excluding samples with ppk duplicates
 software_path="/home/rx32940/miniconda3"
 pirate_path="/scratch/rx32940/pirate"
-# $software_path/scripts/create_pangenome_alignment.pl \
-# -l $pirate_path/all_dated/all_dated_output/genome_no_ppk_duplicates.txt \
-# -i $pirate_path/all_dated/all_dated_output/PIRATE.gene_families.tsv \
-# -f $pirate_path/all_dated/all_dated_output/feature_sequences/ \
-# -o $pirate_path/all_dated/all_dated_output/no_ppk_duplicates_alignment.fasta \
-# -g $pirate_path/all_dated/all_dated_output/no_ppk_duplicates_alignment.gff
 
-module load IQ-TREE/1.6.5-omp
-iqtree -nt AUTO -m MFP -pre $pirate_path/all_dated/iqtree_all_date/iqtree_all_date_nodup_ppk -s $pirate_path/all_dated/all_dated_output/no_ppk_duplicates_alignment.fasta
+# find core genome for isolates w/o ppk gene duplication
+$software_path/bin/PIRATE \
+-i $pirate_path/all_dated/all_dated_ppk/all_dated_core_nodup/all_dated_core_nodup_prokka \
+-o $pirate_path/all_dated/all_dated_ppk/all_dated_core_nodup/all_dated_core_nodup_pirate \
+-a -r -t 12 -pan-opt "-f 6"
+
+# module load IQ-TREE/1.6.5-omp
+# iqtree -nt AUTO -m MFP -pre $pirate_path/all_dated/iqtree_all_date/iqtree_all_date_nodup_ppk -s $pirate_path/all_dated/all_dated_output/no_ppk_duplicates_alignment.fasta
