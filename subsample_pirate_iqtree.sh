a816a8c611a1ae34965e85046d96783e58c90f0c
rx32940 <rx32940@uga.edu>
2020-09-08 16:04:02 -0400

    find core genome with interrogans only

--- subsample_pirate_iqtree.sh
+++ subsample_pirate_iqtree.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -q batch                                                          
-#PBS -N core_subppk_pirate                                       
-#PBS -l nodes=1:ppn=12 -l mem=100gb                                        
+#PBS -q bahl_salv_q                                                          
+#PBS -N interrogans_pirate                                       
+#PBS -l nodes=1:ppn=64 -l mem=100gb                                        
 #PBS -l walltime=300:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
@@ -15,9 +15,9 @@ pirate_path="/scratch/rx32940/pirate"
 
 # find core genome for isolates w/o ppk gene duplication
 $software_path/bin/PIRATE \
--i $pirate_path/all_dated/all_dated_ppk/all_dated_core_nodup/all_dated_core_nodup_prokka \
--o $pirate_path/all_dated/all_dated_ppk/all_dated_core_nodup/all_dated_core_nodup_pirate \
--a -r -t 12 -pan-opt "-f 6"
+-i $pirate_path/interrogans_dated/interrogans_prokka \
+-o $pirate_path/interrogans_dated/interrogans_out \
+-a -r -t 64 -pan-opt "-f 6"
 
 # module load IQ-TREE/1.6.5-omp
 # iqtree -nt AUTO -m MFP -pre $pirate_path/all_dated/iqtree_all_date/iqtree_all_date_nodup_ppk -s $pirate_path/all_dated/all_dated_output/no_ppk_duplicates_alignment.fasta
