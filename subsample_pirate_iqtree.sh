802df75d3c2241335d6058bfc1551b556c8b4079
rx32940 <rx32940@uga.edu>
2020-09-14 12:09:27 -0400

    took out mislabled isolates reran pirate

--- subsample_pirate_iqtree.sh
+++ subsample_pirate_iqtree.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -q bahl_salv_q                                                          
-#PBS -N subppk_core_iqtree                                       
+#PBS -N interrogans_core                                      
 #PBS -l nodes=1:ppn=64 -l mem=100gb                                        
 #PBS -l walltime=500:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
@@ -14,11 +14,11 @@ software_path="/home/rx32940/miniconda3"
 pirate_path="/scratch/rx32940/pirate"
 
 # find core genome for isolates w/o ppk gene duplication
-# $software_path/bin/PIRATE \
-# -i $pirate_path/interrogans_dated/interrogans_prokka \
-# -o $pirate_path/interrogans_dated/interrogans_out \
-# -a -r -t 64 -pan-opt "-f 6"
+$software_path/bin/PIRATE \
+-i $pirate_path/interrogans_dated/interrogans_prokka \
+-o $pirate_path/interrogans_dated/interrogans_out \
+-a -r -t 64 -pan-opt "-f 6"
 
-module load IQ-TREE/1.6.5-omp
-iqtree -nt AUTO -m MFP -pre $pirate_path/all_dated/all_dated_ppk/all_dated_core_nodup/all_dated_core_nodup_iqtree/all_dated_core_nodup \
--s $pirate_path/all_dated/all_dated_ppk/all_dated_core_nodup/all_dated_core_nodup_pirate/core_alignment.fasta
+# module load IQ-TREE/1.6.5-omp
+# iqtree -nt AUTO -m MFP -pre $pirate_path/all_dated/all_dated_ppk/all_dated_core_nodup/all_dated_core_nodup_iqtree/all_dated_core_nodup \
+# -s $pirate_path/all_dated/all_dated_ppk/all_dated_core_nodup/all_dated_core_nodup_pirate/core_alignment.fasta
