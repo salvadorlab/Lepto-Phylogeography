35001fc5c4e33424ca696476cc745557e775c47c
rx32940 <rx32940@uga.edu>
2020-09-17 11:07:32 -0400

    change queue

--- subsample_pirate_iqtree.sh
+++ subsample_pirate_iqtree.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -q batch                                                          
+#PBS -q bahl_salv_q                                                          
 #PBS -N interrogans_iqtree_aa                                      
-#PBS -l nodes=1:ppn=24 -l mem=100gb                                        
+#PBS -l nodes=1:ppn=64 -l mem=100gb                                        
 #PBS -l walltime=500:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
