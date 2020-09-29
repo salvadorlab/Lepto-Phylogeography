5906a3a62fca8d7306d3157e1473a288ba8fdae6
rx32940 <rx32940@uga.edu>
2020-09-29 16:56:49 -0400

    testing fastGear to get pop struc and recombination between lineages

--- run_fastGear.sh
+++ run_fastGear.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -q batch                                                          
+#PBS -q bahl_salv_q                                                          
 #PBS -N fastGear                                      
-#PBS -l nodes=1:ppn=12 -l mem=50gb                                        
+#PBS -l nodes=1:ppn=24 -l mem=50gb                                        
 #PBS -l walltime=500:00:00                                                
 #PBS -M rx32940@uga.edu                                                  
 #PBS -m abe                                                              
@@ -10,10 +10,10 @@
 #PBS -j oe
 
 software_path="/home/rx32940/fastGEARpackageLinux64bit"
-matlab_path="/usr/local/apps/MATLAB/R2019b"
+# need to install MCR interactive instead of silent 
+# change installation destination folder
+matlab_path="/home/rx32940/MATLAB/v901" 
 file_path="/scratch/rx32940/pirate/interrogans_dated"
-
-module load matlab/R2019b
 
 $software_path/run_fastGEAR.sh $matlab_path \
 $file_path/interrogans_out/core_alignment.fasta \
