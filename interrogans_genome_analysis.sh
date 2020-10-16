5850af35464c267eaf24a7249e0d44881482904d
rx32940 <rx32940@uga.edu>
2020-10-15 20:10:00 -0400

    running iqtree for fastGear plotting in python

--- interrogans_genome_analysis.sh
+++ interrogans_genome_analysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-## PBS -N test_fastGear                                       
-## PBS -l nodes=1:ppn=1 -l mem=100gb                                        
+## PBS -N all_int_iqtree                                      
+## PBS -l nodes=1:ppn=64 -l mem=100gb                                        
 ## PBS -l walltime=200:00:00                                                
 ## PBS -M rx32940@uga.edu                                                  
 ## PBS -m abe                                                              
@@ -26,6 +26,12 @@ file_path="/scratch/rx32940/interrogans_genome/pirate/feature_sequences"
 # # parse PIRATE outputs 
 # perl PIRATE_to_roary.pl -i /scratch/rx32940/interrogans_genome/pirate/PIRATE.*.tsv -o /scratch/rx32940/interrogans_genome/pirate/roary_presence_absence
 
+# # recontruct the ML tree with core genome concatenation produced by PIRATE
+# # this will be used in python code for plot fastGear results
+module load IQ-TREE/1.6.5-omp
+iqtree -nt AUTO -m MFP -pre $dir_path/iqtree/all_int_core \
+-s $dir_path/pirate/core_alignment.fasta
+
 # after filtered genes base on : https://github.com/salvadorlab/Lepto-Phylogeography/issues/6#issuecomment-705812907
 # run fastGear on filtered conservative genes
 # splited the 8135 genes with avg dosage <= 1.25 in to 41 files with 200 gene id per file
@@ -33,49 +39,52 @@ file_path="/scratch/rx32940/interrogans_genome/pirate/feature_sequences"
 # mkdir folders for each gene in the fastGear folder
 
     
-script_path="dir_path=\"/scratch/rx32940/interrogans_genome\"\n
-software_path=\"/home/rx32940/miniconda3\"\n
-fastGear_path=\"/home/rx32940/fastGEARpackageLinux64bit\"\n
-matlab_path=\"/home/rx32940/MATLAB/v901\"\n
-file_path=\"/scratch/rx32940/interrogans_genome/pirate/feature_sequences\"\n
-LD_LIBRARY_PATH=/home/rx32940/MATLAB/v901/runtime/glnxa64:/home/rx32940/MATLAB/v901/bin/glnxa64:/home/rx32940/MATLAB/v901/sys/os/glnxa64:/home/rx32940/MATLAB/v901/sys/opengl/lib/glnxa64\n"
+# script_path="dir_path=\"/scratch/rx32940/interrogans_genome\"\n
+# software_path=\"/home/rx32940/miniconda3\"\n
+# fastGear_path=\"/home/rx32940/fastGEARpackageLinux64bit\"\n
+# matlab_path=\"/home/rx32940/MATLAB/v901\"\n
+# file_path=\"/scratch/rx32940/interrogans_genome/pirate/feature_sequences\"\n
+# LD_LIBRARY_PATH=/home/rx32940/MATLAB/v901/runtime/glnxa64:/home/rx32940/MATLAB/v901/bin/glnxa64:/home/rx32940/MATLAB/v901/sys/os/glnxa64:/home/rx32940/MATLAB/v901/sys/opengl/lib/glnxa64\n"
     
-for list in $dir_path/x*;
-do
-    echo "create script for $list"
-    (
-    echo -e 
-    job_header="#!/bin/bash\n
-    #PBS -q batch\n
-    #PBS -N ${list}_fastGear\n
-    #PBS -l nodes=1:ppn=1 -l mem=30gb\n
-    #PBS -l walltime=200:00:00\n
-    #PBS -M rx32940@uga.edu\n
-    #PBS -m abe\n
-    #PBS -o /scratch/rx32940\n
-    #PBS -e /scratch/rx32940\n
-    #PBS -j oe\n"
+# for list in $dir_path/x*;
+# do
+#     echo "create script for $list"
+#     (
+#     echo -e 
+#     job_header="#!/bin/bash\n
+#     #PBS -q batch\n
+#     #PBS -N ${list}_fastGear\n
+#     #PBS -l nodes=1:ppn=1 -l mem=30gb\n
+#     #PBS -l walltime=200:00:00\n
+#     #PBS -M rx32940@uga.edu\n
+#     #PBS -m abe\n
+#     #PBS -o /scratch/rx32940\n
+#     #PBS -e /scratch/rx32940\n
+#     #PBS -j oe\n"
 
-    echo -e "$job_header" > $dir_path/submit_sub_fastGear.sh
+#     echo -e "$job_header" > $dir_path/submit_sub_fastGear.sh
 
-    echo -e "$script_path" >> $dir_path/submit_sub_fastGear.sh
+#     echo -e "$script_path" >> $dir_path/submit_sub_fastGear.sh
 
-    echo -e "cat $list | xargs -I{} \
-    $fastGear_path/run_fastGEAR.sh $matlab_path \
-    $file_path/{}.nucleotide.fasta \
-    $dir_path/fastGear/{}/interrogans_{}.mat \
-    $fastGear_path/fG_input_specs.txt" >> $dir_path/submit_sub_fastGear.sh
+#     echo -e "cat $list | xargs -I{} \
+#     $fastGear_path/run_fastGEAR.sh $matlab_path \
+#     $file_path/{}.nucleotide.fasta \
+#     $dir_path/fastGear/{}/interrogans_{}.mat \
+#     $fastGear_path/fG_input_specs.txt" >> $dir_path/submit_sub_fastGear.sh
 
-    qsub $dir_path/submit_sub_fastGear.sh
+#     qsub $dir_path/submit_sub_fastGear.sh
     
-    echo -e "submitted script for $list"
-    ) &
-    echo "waiting"
-    wait
-done
+#     echo -e "submitted script for $list"
+#     ) &
+#     echo "waiting"
+#     wait
+# done
 
-# # plot base on original sample order
-# $fastGear_path/run_plotRecombinations.sh $matlab_path $dir_path/fastGear/interrogans_g00295.mat 1 1
+# # # plot scatterplot for all genes in pan genome
+# python post_fastGEAR.py \
+# -i $dir_path/fastGear/ \
+# -o $dir_path/fastGear_out/all_int_recomb \
+# -s -f pdf -z False 
 
 # run snippy- generate script
 # snippy_input.tab generation refer to github issue
