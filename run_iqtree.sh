6be3ae8e14cd8f7aff943aca80e69449bb2b150a
rx32940 <rx32940@uga.edu>
2021-02-10 12:00:21 -0500

    run iqtree

--- run_iqtree.sh
+++ run_iqtree.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-#PBS -q batch                                                            
-#PBS -N iq_core_pirate                                    
-#PBS -l nodes=1:ppn=24 -l mem=100gb                                        
-#PBS -l walltime=300:00:00                                                
-#PBS -M rx32940@uga.edu                                                  
-#PBS -m abe                                                              
-#PBS -o /scratch/rx32940                        
-#PBS -e /scratch/rx32940                        
-#PBS -j oe
+#SBATCH --partition=batch
+#SBATCH --job-name=fastGear_maskedSNP_iqtree
+#SBATCH --ntasks=1                    	
+#SBATCH --cpus-per-task=10             
+#SBATCH --time=100:00:00
+#SBATCH --mem=100G
+#SBATCH --output=/scratch/rx32940/fastGear_maskedSNP_iqtree.%j.out       
+#SBATCH --error=/scratch/rx32940/fastGear_maskedSNP_iqtree.%j.out        
+#SBATCH --mail-user=rx32940@uga.edu
+#SBATCH --mail-type=ALL 
 
 module load IQ-TREE/1.6.5-omp
 
 # use MFP: model finder to find the right substitution model
 # -nt AUTO, detects best number of cores to use
-iqtree -nt AUTO -m MFP -pre /scratch/rx32940/pirate/iqtree_mi6/iqtree_mi6 -s /scratch/rx32940/pirate/dated_output_mi6/core_alignment.fasta -o SAMN02947961
+iqtree -nt AUTO -m MFP+ASC \
+-pre /scratch/rx32940/all_interrogans_dated/fastgear/fastGear_iqtree/all_int_dated_core_mask_snps \
+-s /scratch/rx32940/all_interrogans_dated/fastgear/all_int_dated_core_mask_snps.fasta
