9e0b821b078faf7f7927723e915e7a659c2094b9
rx32940 <rx32940@uga.edu>
2021-02-10 16:00:22 -0500

    run fastBAPs

--- fastBAP.r
+++ fastBAP.r
@@ -7,13 +7,13 @@ library(ape)
 library(phytools)
 library(ggtree)
 
-setwd("/scratch/rx32940/interrogans_genome/pirate_sero/")
+setwd("/scratch/rx32940/all_interrogans_dated/")
 
-sparse.data <- import_fasta_sparse_nt("out/core_alignment.fasta")
+sparse.data <- import_fasta_sparse_nt("roary/core_gene_alignment.aln")
 sparse.data <- optimise_prior(sparse.data, type = "baps")
 baps.hc <- fast_baps(sparse.data)
 best.partition <- best_baps_partition(sparse.data, baps.hc)
-iqtree <- read.newick( "../iqtree/int_sero_iqtree.newick")
+iqtree <- read.newick( "iqtree/all_interrogans_dated.treefile")
 plot.df <- data.frame(id = colnames(sparse.data$snp.matrix), fastbaps = best.partition, 
     stringsAsFactors = FALSE)
 gg <- ggtree(iqtree)
@@ -22,4 +22,4 @@ f2 <- facet_plot(gg, panel = "fastbaps", data = plot.df, geom = geom_tile, aes(x
 ggsave("core_genome_fastbaps.pdf")
 
 baps_pirate <- data.frame(best.partition) %>% mutate(BioSample.Accession = rownames(.)) %>% rename(pirate_baps = best.partition )
-write.csv(baps_pirate, "pirate_baps_for_each_acc.csv")
+write.csv(baps_pirate, "pirate_baps_for_each_acc.csv", quote = FALSE, header = FALSE)
