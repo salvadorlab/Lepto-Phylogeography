a682a6397ed92d1d665cfc0699227af73fc2db09
rx32940 <rx32940@uga.edu>
2021-02-15 14:33:45 -0500

    commit

--- run_mafft.sh
+++ run_mafft.sh
@@ -5,12 +5,12 @@
 #SBATCH --cpus-per-task=10             
 #SBATCH --time=100:00:00
 #SBATCH --mem=100G
-#SBATCH --output=/scratch/rx32940/default_mafft_covid500random.%j.out       
-#SBATCH --error=/scratch/rx32940/default_mafft_covid500random.%j.out        
+#SBATCH --output=/scratch/rx32940/mafft_covid500random.%j.out       
+#SBATCH --error=/scratch/rx32940/mafft_covid500random.%j.out        
 #SBATCH --mail-user=rx32940@uga.edu
 #SBATCH --mail-type=ALL 
 
 module load MAFFT/7.470-GCC-8.3.0-with-extensions
 
 data_path="/home/rx32940/github/washingtonCountiesCovid19/data/rawData/GeneticData"
-mafft $data_path/gisaid_hcov-19_2021_02_15_17.fasta > $data_path/covid_random500_alignment02152021.fasta 
+mafft --globalpair --maxiterate 1000 $data_path/gisaid_hcov-19_2021_02_15_17.fasta > $data_path/covid_random500_alignment02152021_careful.fasta 
